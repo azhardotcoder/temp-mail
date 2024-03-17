ea1cc47a34494f473cf8c4297ab3d4ddb1001fa0
Azhar Khan <azar123khan456@gmail.com>
2024-03-18 04:51:31 +0530

    init

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://temp-mail44.p.rapidapi.com/api/v3/email/tz1uxahdr5aj@rentforsale7.comc/messages",
+    headers: {
+      "X-RapidAPI-Key": process.env.API_SECRET,
+      "X-RapidAPI-Host": process.env.RAPIDAPI_HOST
+    },
+  };
+
+  async function readEmail() {
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      res.send(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  readEmail();
+});
+
+app.post("/email", (req, res) => {
+  const options = {
+    method: "POST",
+    url: "https://temp-mail44.p.rapidapi.com/api/v3/email/new",
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": process.env.API_SECRET,
+      "X-RapidAPI-Host": process.env.RAPIDAPI_HOST
+    },
+    data: {
+      key1: "value",
+      key2: "value",
+    },
+  };
+
+  async function fetchData() {
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      res.send(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  fetchData();
+});
+app.listen(PORT, () => {
+  console.log(PORT);
+});
