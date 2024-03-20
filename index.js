3612f0e1d7526d3a46f43e85ce31a8d74132e0c5
Azhar Khan <azar123khan456@gmail.com>
2024-03-21 01:39:07 +0530

    bug fixed

--- index.js
+++ index.js
@@ -13,12 +13,21 @@ const PORT = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
   const { 'x-rapidapi-key': rapidapiKey, 'x-rapidapi-host': rapidapiHost } = req.headers;
+  const {email}=req.body
+  console.log(email);
   if(!(rapidapiKey&&rapidapiHost)){
     res.status(401).send({message: 'unauthorized user'})
+    return
   }
+  if(!email){
+    res.status(400).send({message: 'email required'})
+    return
+  }
+  // res.send("ping pong")
+  // return
   const options = {
     method: "GET",
-    url: "https://temp-mail44.p.rapidapi.com/api/v3/email/tz1uxahdr5aj@rentforsale7.com/messages",
+    url: `https://temp-mail44.p.rapidapi.com/api/v3/email/${email}/messages`,
     headers: {
       "X-RapidAPI-Key": rapidapiKey,
       "X-RapidAPI-Host": rapidapiHost
@@ -40,7 +49,12 @@ app.get("/", (req, res) => {
 });
 
 app.post("/email", (req, res) => {
+  const { 'x-rapidapi-key': rapidapiKey2, 'x-rapidapi-host': rapidapiHost2 } = req.headers;
   const {key1, key2}=req.body
+console.log(req.body);
+// console.log(rapidapiKey2, rapidapiHost2);
+  // res.send("OK OK")
+  // return
   const { 'x-rapidapi-key': rapidapiKey, 'x-rapidapi-host': rapidapiHost } = req.headers;
   if(!(rapidapiKey&&rapidapiHost)){
     res.status(401).send({message: 'unauthorized user'})
